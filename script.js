fa003ef1d4b187c01d88e37cf50e2807bd4c93b7
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-22 14:41:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-//your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
 
 const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+  { url: 'https://example.com/image1.jpg', description: 'Image 1' },
+  { url: 'https://example.com/image2.jpg', description: 'Image 2' },
+  { url: 'https://example.com/image3.jpg', description: 'Image 3' },
 ];
+function Image(images) {
+  const imagePromises = images.map(image => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => {
+        resolve(img);
+      };
+      img.onerror = () => {
+        reject(`Failed to load image's URL: ${image.url}`);
+      };
+      img.src = image.url;
+    });
+  });
+
+  Promise.all(imagePromises)
+    .then(images => {	
+      const output = document.getElementById('output');
+      images.forEach(image => {
+        const imgElement = document.createElement("Downloads and displays images");
+        imgElement.src = image.src;
+        imgElement.alt = image.description;
+        imgElement.width = 400;
+        imgElement.height = 300;
+        output.appendChild(imgElement);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+const downloadButton = document.getElementById('download-images-button');
+downloadButton.addEventListener('click', () => {
+  Images(images);
+});
