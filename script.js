6ad5892bde52e5a2c2e0eb6d49b370f6cd4b0d3b
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-22 14:58:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 const images = [
-  { url: 'https://example.com/image1.jpg', description: 'Image 1' },
-  { url: 'https://example.com/image2.jpg', description: 'Image 2' },
-  { url: 'https://example.com/image3.jpg', description: 'Image 3' },
+  { url: 'https://picsum.photos/400/300', description: 'Image 1' },
+  { url: 'https://picsum.photos/400/300', description: 'Image 2' },
+  { url: 'https://picsum.photos/400/300', description: 'Image 3' },
 ];
 function Images(images) {
   const imagePromises = images.map(image => {
@@ -22,8 +21,8 @@ function Images(images) {
     .then(images => {	
       const output = document.getElementById('output');
       images.forEach(image => {
-        const imgElement = document.createElement("Downloads and displays images");
-        imgElement.src = image.src;
+        const imgElement = document.createElement("img");
+        imgElement.src = image.url;
         imgElement.alt = image.description;
         imgElement.width = 400;
         imgElement.height = 300;
